b6476181ce03a1c59dcadaedaf78b5ecda06c38b
avi9360 <savinash406@gmail.com>
2021-11-23 20:27:34 +0530

    Rate limit is added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const port = 3001;
 const fast2sms = require("fast-two-sms");
 
+const db_phone = [];
+
 app.listen(port, () => console.log(`SMS app listening on port ${port}!`));
 
 app.use(express.json());
@@ -16,12 +18,17 @@ app.get("/", (req, res) => {
 
 app.post("/sms", async (req, res) => {
   const { phone, message } = req.body;
+  if (db_phone.find(x => x === phone)) {
+    return res.status(409).send("This Student is already present in the BUS");
+  }
   const options = {
     authorization: process.env.SMS_API_KEY,
     message: message,
     numbers: [phone]
   };
   const response = await fast2sms.sendMessage(options);
+  db_phone.push(phone);
   if (response.return) res.status(200).send(response);
   else res.status(500).send(response);
+  console.log(db_phone);
 });
